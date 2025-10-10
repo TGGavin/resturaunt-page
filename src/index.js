3b45d478a514e02a2de21c64a42584bbb9aafbdc
TGGavin <178350968+TGGavin@users.noreply.github.com>
2025-10-09 20:59:02 -0700

    Replace renderPage function with Page. Add new Render function within Page

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@ import "./styles.css";
 import applepieImg from "./img/apple-pie.jpg"
 import pancakeImg from "./img/pancakes.jpg"
 
-const renderPage = function () {
+const Page = function () {
 
-    const content = document.querySelector("#content");
+    const Render = function () {
 
+        const content = document.querySelector("#content");
+        
         const home = function () {
             const resTitle = document.createElement("h1");
             resTitle.classList.add("res-title");
             resTitle.textContent = "Restaurant Title";
-
+            
             const splashCont = document.createElement("div");
             splashCont.classList.add("splash-cont");
-
+            
             const imgWrapper1 = document.createElement("div");
             imgWrapper1.classList.add("img-wrapper");
             const imgWrapper2 = document.createElement("div");
@@ -24,12 +26,12 @@ const renderPage = function () {
             imgTitle1.textContent = titleTxt;
             const imgTitle2 = document.createElement("h2");
             imgTitle2.textContent = titleTxt;
-
+            
             const splashImg1 = document.createElement("img");
             splashImg1.src = applepieImg;
             const splashImg2 = document.createElement("img");
             splashImg2.src = pancakeImg;
-
+            
             content.appendChild(resTitle);
             content.appendChild(splashCont);
             splashCont.appendChild(imgWrapper1)
@@ -39,8 +41,11 @@ const renderPage = function () {
             imgWrapper1.appendChild(splashImg1)
             imgWrapper2.appendChild(splashImg2)
         }
-
+        
         function clean() {
             content.innerHTML = "";
         }
+
+
     }
+}
